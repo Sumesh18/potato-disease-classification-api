96704a92c05881a8ecb1794df107a9b68e974389
Sumesh Tungaturthi <109428489+Sumesh18@users.noreply.github.com>
2025-09-21 13:25:56 +0530

    updated port number in main.py

--- api/main.py
+++ api/main.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 from fastapi.middleware.cors import CORSMiddleware
 
 import os
+
 app = FastAPI()
 
 
@@ -21,9 +22,11 @@ app.add_middleware(
 MODEL = tf.keras.models.load_model("../models/1.keras")
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 
+
 @app.get("/ping")
 async def ping():
     return {"ping": "pong!"}
+
 
 def read_file_as_image(data) -> np.ndarray:
     image = np.array(Image.open(BytesIO(data)))
@@ -31,18 +34,16 @@ def read_file_as_image(data) -> np.ndarray:
 
 
 @app.post("/predict")
-async def predict(
-    file: UploadFile = File(...)
-):
+async def predict(file: UploadFile = File(...)):
     image = read_file_as_image(await file.read())
     img_batch = np.expand_dims(image, 0)
     prediction = MODEL.predict(img_batch)
     predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
     confidence = np.max(prediction[0])
-    return {
-        "class": predicted_class,
-        "confidence": float(confidence)
-    }
+    return {"class": predicted_class, "confidence": float(confidence)}
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    # uvicorn.run(app, host="localhost", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
